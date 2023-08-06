dad390cb3d3eb88059e0a41b9a3ebf10198939f3
LucianoR23 <231097lr@gmail.com>
2023-08-06 15:43:12 -0300

    Pruebas sobre react

--- tests/base-pruebas/08-imp-exp.test.js
+++ tests/base-pruebas/08-imp-exp.test.js
@@ -1,4 +1,4 @@
-import { getHeroeById } from "../../src/base-pruebas/08-imp-exp";
+import { getHeroeById, getHeroesByOwner } from "../../src/base-pruebas/08-imp-exp";
 
 
 describe('Pruebas en 08-imp-exp', () => {
@@ -15,12 +15,54 @@ describe('Pruebas en 08-imp-exp', () => {
         })
 
     })
+
     test('getHeroeById debe retornar undefined si no existe', () => {
         
         const id = 100;
         const hero = getHeroeById( id )
 
         expect( hero ).toBe(undefined)
+
+    })
+
+    test('getHeroesByOwner debe retornar un arreglo con heroes por owner', () => {
+        
+        const owner = 'DC'
+        const owner2 = 'Marvel'
+        const marvel = [
+            {
+                id: 2,
+                name: 'Spiderman',
+                owner: 'Marvel'
+            },
+            {
+                id: 5,
+                name: 'Wolverine',
+                owner: 'Marvel'
+            }]
+
+        const test = getHeroesByOwner(owner)
+
+        expect(test.length).toBe(3)
+        expect(test).toEqual([{
+            id: 1,
+            name: 'Batman',
+            owner: 'DC'
+        },
+        {
+            id: 3,
+            name: 'Superman',
+            owner: 'DC'
+        },
+        {
+            id: 4,
+            name: 'Flash',
+            owner: 'DC'
+        }])
+
+        expect(getHeroesByOwner(owner2).length).toBe(2)
+        expect(getHeroesByOwner(owner2)).toEqual(marvel)
+
 
     })
 
